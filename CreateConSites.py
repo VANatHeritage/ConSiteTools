5a910e86fe7fe50250ed255d61cddbe581db4e06
Kirsten Hazler <kirsten.hazler@dcr.virginia.gov>
2021-08-02 10:22:13 -0400

    Replaced errant tabs w/ spaces bc Python

--- CreateConSites.py
+++ CreateConSites.py
@@ -2,7 +2,7 @@
 # CreateConSites.py
 # Version:  ArcGIS 10.3.1 / Python 2.7.8
 # Creation Date: 2016-02-25
-# Last Edit: 2021-07-29
+# Last Edit: 2021-08-02
 # Creator:  Kirsten R. Hazler
 
 # Summary:
@@ -1164,8 +1164,8 @@ def CreateNoBuffSBB(in_PF, out_SBB):
    try:
       # Process: Select (No-Buffer Rules)
       ## selQry = "(intRule in (-1,13,15) AND (fltBuffer = 0))"
-		## Above selection query replace 7/29/21 
-		selQry = "(intRule in (-1,1,2,3,4,8,10,11,12,13,14,15) AND (fltBuffer = 0))"
+      ## Above selection query replaced 7/29/21 
+      selQry = "(intRule in (-1,1,2,3,4,8,10,11,12,13,14,15) AND (fltBuffer = 0))"
       arcpy.MakeFeatureLayer_management(in_PF, "tmpLyr", selQry)
 
       # Count records and proceed accordingly
